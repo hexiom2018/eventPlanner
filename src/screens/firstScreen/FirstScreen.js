b2a365355ac463e77036ec3d2b91e11d803afbac
nabeelRrehman <nabeelshahid2@gmail.com>
2019-04-09 20:16:33 +0500

    main screen styling

--- src/screens/firstScreen/FirstScreen.js
+++ src/screens/firstScreen/FirstScreen.js
@@ -6,22 +6,154 @@ class FirstScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            card:
+                [
+                    {
+                        name: 'Cattle Farm New Zealand',
+                        price: 2000,
+                        quantity: 1
+                    },
+                    {
+                        name: 'Corn Field Vietnam',
+                        price: 500,
+                        quantity: 1
+                    }
+                ],
+            total: 0
         }
     }
+
+    quantityTotal() {
+        const { card } = this.state
+        var myPrice = 0
+        card.map((items) => {
+            myPrice += items.price * items.quantity
+        })
+
+        this.setState({
+            total: myPrice
+        })
+    }
+
+    componentWillMount() {
+
+        this.quantityTotal()
+    }
+
+    subtract(items, index) {
+        const { card } = this.state
+        if (items.quantity > 1) {
+            card[index].quantity = card[index].quantity - 1
+
+            this.setState({ card }, () => {
+                this.quantityTotal()
+            })
+        }
+    }
+
+    add(items, index) {
+        const { card } = this.state
+        if (items.quantity >= 1) {
+            card[index].quantity = card[index].quantity + 1
+
+            this.setState({ card }, () => {
+                this.quantityTotal()
+            })
+        }
+    }
+
+    myCard(items, index) {
+        return (
+            <div className={'card'}>
+                <div className={'pic'}>
+                    <img src={Cattle} style={{ width: '100%', height: '100%' }} alt="Logo" />
+                </div>
+                <div>
+                    <div style={{ flexBasis: '80%', padding: '0px 10px', color: 'grey' }}>
+                        {items.name}
+                    </div>
+                    <div style={{ padding: '10px' }} className={'quantity'}>
+                        <div style={items.quantity === 1 ? { backgroundColor: 'lightgrey', color: 'white' } : null} onClick={() => this.subtract(items, index)}>
+                            {'-'}
+                        </div>
+                        <div>
+                            {items.quantity}
+                        </div>
+                        <div onClick={() => this.add(items, index)}>
+                            {'+'}
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {items.price}
+                </div>
+            </div>
+        )
+    }
+
+    accountDetails() {
+        const { total } = this.state
+        return (
+            <div className={'details'}>
+                <div className={'account'}>
+                    Account Details
+                        </div>
+                <div style={{ display: 'flex', flexBasis: '100%', flexWrap: 'wrap', padding: '10px' }}>
+                    <div style={{ flexGrow: 1, fontWeight: 'bold', color: 'black', alignSelf: 'center' }}>
+                        Bank Account:
+                            </div>
+                    <div style={{ textAlign: 'center' }}>
+                        <div>
+                            SEB 7987654777
+                                </div>
+                        <div>
+                            Recipient HarvestMoon Inc
+                                </div>
+                    </div>
+                </div>
+                <div style={{ display: 'flex', flexBasis: '100%', flexWrap: 'wrap', padding: '10px' }}>
+                    <div style={{ flexGrow: 1, fontWeight: 'bold', color: 'black' }}>
+                        Deadline:
+                            </div>
+                    <div>
+                        01 April 2019, 18.00
+                            </div>
+                </div>
+                <div style={{ display: 'flex', flexBasis: '100%', flexWrap: 'wrap', padding: '10px' }}>
+                    <div style={{ flexGrow: 1, fontWeight: 'bold', color: 'black' }}>
+                        Amount to transfer:
+                            </div>
+                    <div style={{ color: 'orange' }}>
+                        {`$ ${total}`}
+                    </div>
+                </div>
+                <div className={'button'}>
+                    <button>
+                        Confirm
+                            </button>
+                </div>
+            </div>
+        )
+    }
+
     render() {
-        const { all } = this.state
+        const { card } = this.state
         return (
             <div className="main-div">
-                <div className="first-div-after-main">
-               <div className="picture-dic">
-               <img src={Cattle} alt="Logo" className="cattle-image" />
-               </div>
-               <div className="tittle-div"></div>
-               <div className="price-div"></div>
-                
+                <div className={'left-content'}>
+                    {
+                        card &&
+                        card.map((items, index) => {
+                            return (
+                                this.myCard(items, index)
+                            )
+                        })
+                    }
                 </div>
-                <div className="second-div-after-main">
-
+                <div className={'right-content'}>
+                    {
+                        this.accountDetails()
+                    }
                 </div>
             </div>
         )
